f5065be0d76087a849a369eab931fadab47b9589
chakchakk <ioann.chimrov@gmail.com>
2022-08-16 23:12:39 +0300

    All tests passed + refactor

--- homework.py
+++ homework.py
@@ -122,7 +122,8 @@ class Swimming(Training):
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        return self.length_pool * self.count_pool / self.M_IN_KM / self.duration
+        first_div = self.length_pool * self.count_pool
+        return first_div / self.M_IN_KM / self.duration
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
